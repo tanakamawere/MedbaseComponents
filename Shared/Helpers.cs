3d80042ddd3e4a9a17bc9adef617bdf8b06cc0fe
Tanaka Mawere <tanaka@tanakamawere.co.zw>
2024-04-02 09:07:00 +0200

    added profile page and change password screen

---
+++ Shared/Helpers.cs
@@ -0,0 +1,11 @@
+﻿using MudBlazor;
+
+namespace MedbaseComponents.Shared;
+
+public class Helpers
+{
+    public static void ShowSnackbar(string message, Severity severity, ISnackbar Snackbar)
+    {
+        _ = Snackbar.Add(message, severity);
+    }
+}
