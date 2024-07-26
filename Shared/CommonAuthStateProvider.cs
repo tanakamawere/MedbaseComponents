96da4a8d3c4690a1c0fae63592605170e78b71c2
Tanaka Mawere <tanaka@tanakamawere.co.zw>
2024-07-26 10:52:21 +0200

    commit before trying auth0

---
+++ Shared/CommonAuthStateProvider.cs
@@ -0,0 +1,67 @@
+﻿using MedbaseLibrary.Helpers;
+using MedbaseLibrary.MsalClient;
+using MedbaseLibrary.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace MedbaseComponents.Shared;
+
+public class CommonAuthStateProvider : AuthenticationStateProvider
+{
+    private IAuthMemory authMemory;
+    public CommonAuthStateProvider(IAuthMemory auth)
+    {
+        authMemory = auth;
+    }
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        string token = "";
+        AuthenticationState result = new(new ClaimsPrincipal(new ClaimsIdentity()));
+        try
+        {
+            //token = authMemory.GetToken().Result;
+            if (string.IsNullOrEmpty(token))
+            {
+                return result;
+            }
+            else
+            {
+                result = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(TokenToClaims(token), "JwtBearer")));
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return result;
+    }
+
+    private IEnumerable<Claim> TokenToClaims(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        JwtSecurityToken jwtSecurityToken;
+
+        jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+
+        var claims = jwtSecurityToken.Claims;
+
+        return claims;
+    }
+
+    public void MarkUserAsAuthenticated(string token)
+    {
+        var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(TokenToClaims(token), "JwtBearer"));
+        var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+        NotifyAuthenticationStateChanged(authState);
+
+    }
+
+    public void NotifyUserAuthentication(ClaimsPrincipal user)
+    {
+        var authState = Task.FromResult(new AuthenticationState(user));
+        NotifyAuthenticationStateChanged(authState);
+    }
+}
